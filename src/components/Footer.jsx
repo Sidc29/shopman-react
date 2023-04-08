895d6e10bc9e65edbf4959c16ed2994b69fee8c2
Siddharth Choudhari <choudharishiddharth@gmail.com>
2023-04-08 21:31:03 +0530

    add favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="bg-[#4D4D4D] p-4 text-center">
-      <span className="text-white text-sm opacity-70">Made by Siddharth</span>
+      <span className="text-white text-sm opacity-50">Made by </span>
+      <a
+        href="http://shiddharth-portfolio.vercel.app/"
+        className="text-white text-sm opacity-50 hover:opacity-100 font-medium"
+      >
+        Siddharth
+      </a>
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,41 @@ import { useCart } from "../context/CartContext";
 
 export const Header = () => {
   const { cartList } = useCart();
+
+  const activeClass =
+    "text-lg text-slate-100 py-1.5 px-3 rounded-lg hover:bg-slate-400 bg-slate-400 bg-opacity-10 hover:bg-opacity-10";
+  const inactiveClass =
+    "text-lg text-slate-100 py-1.5 px-3 rounded-lg hover:bg-slate-400 hover:bg-opacity-10";
+
   return (
     <header className="lg:max-w-[80%] lg:mx-auto">
       <nav className="flex flex-row justify-between items-center p-3 bg-[#4D4D4D] lg:rounded-b-lg shadow-lg">
         <Link to="/" className="flex flex-row items-center gap-3">
           <img
-            src="https://static-00.iconduck.com/assets.00/cart-shopping-list-icon-493x512-fh2rzzxm.png"
+            src="src/assets/cart-logo.png"
             alt="shopping cart image"
             className="w-8 h-auto"
           />
           <span className="text-xl font-semibold text-slate-100">ShopMan</span>
         </Link>
-        <ul className="flex flex-row gap-2 max-sm:hidden">
-          <li className="text-lg text-slate-100 py-1.5 px-3 rounded-lg hover:bg-slate-400 hover:bg-opacity-10">
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li className="text-lg text-slate-100 py-1.5 px-3 rounded-lg hover:bg-slate-400 hover:bg-opacity-10">
-            <NavLink to="/cart">Cart</NavLink>
-          </li>
-        </ul>
+        <div className="flex flex-row gap-2 max-sm:hidden">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? activeClass : inactiveClass
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              isActive ? activeClass : inactiveClass
+            }
+          >
+            Cart
+          </NavLink>
+        </div>
         <Link to="/cart" className="text-lg text-slate-100">
           <ShoppingCartIcon />: {cartList.length}
         </Link>
